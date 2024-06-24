5395dcc38f86271653be3c034adf78b4641383c8
Ramon Valentin <101585464+RamonvCS@users.noreply.github.com>
2024-06-24 13:18:01 -0400

    views and index hmtl

--- webapp/views.py
+++ webapp/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 
-# Creamos el Http response
-from django.http import HttpResponse
-
 # Creamos la funcion de Home para el HomePage
 def home(request): 
 
-    # return HttpResponse('Hello World')
     # llamamos de template el template creado llamado index.html 
     return render(request, 'webapp/index.html')
+
