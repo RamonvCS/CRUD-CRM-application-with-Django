b88f7d4e610399dc3bb2122a00746595361aeebf
Ramon Valentin <101585464+RamonvCS@users.noreply.github.com>
2024-06-24 16:20:00 -0400

    register page started adding <h1> and <h3> with content inside

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
 
 # Creamos la funcion de Home para el HomePage
 def home(request): 
@@ -6,3 +7,21 @@ def home(request):
     # llamamos de template el template creado llamado index.html 
     return render(request, 'webapp/index.html')
 
+# Creamos la funcion de Register
+def register(request):
+
+    form = CreateUserForm()
+
+    if request.method == "POST":
+
+        form = CreateUserForm(request.POST) 
+
+        if form.is_valid():
+
+            form.save()
+
+            # return redirect('')
+
+    context = {'form': form}
+
+    return render(request, 'webapp/register.html', context=context)
