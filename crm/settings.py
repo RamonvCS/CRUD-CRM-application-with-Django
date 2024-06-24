9711aa92ffa2a5e7ff9bd6c61fe7120c3bb4f8c1
Ramon Valentin <101585464+RamonvCS@users.noreply.github.com>
2024-06-24 11:32:11 -0400

    index.html <h1> created

--- crm/settings.py
+++ crm/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'crm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'crm/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
