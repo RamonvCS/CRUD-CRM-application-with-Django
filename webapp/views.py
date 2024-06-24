e0a5a183c5bb97c5654135a613d5f05ddcc0b03b
Ramon Valentin <101585464+RamonvCS@users.noreply.github.com>
2024-06-24 17:17:11 -0400

    login coding started

--- webapp/views.py
+++ webapp/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
 
-# Creamos la funcion de Home para el HomePage
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+
+#----------- Creamos la funcion de Home para el HomePage
 def home(request): 
 
     # llamamos de template el template creado llamado index.html 
     return render(request, 'webapp/index.html')
 
-# Creamos la funcion de Register
+# ----------Creamos la funcion de Register
 def register(request):
 
     form = CreateUserForm()
@@ -25,3 +28,28 @@ def register(request):
     context = {'form': form}
 
     return render(request, 'webapp/register.html', context=context)
+
+# ---------- Login a user
+def my_login(request):
+
+    form = LoginForm():
+
+    if request.method == "POST":
+
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = request>POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                # return redirect('')
+
+
+
