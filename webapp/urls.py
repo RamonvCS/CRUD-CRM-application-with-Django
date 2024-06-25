e11d53268ad41a40bbf876dab81ec3d2084b3db5
Ramon Valentin <101585464+RamonvCS@users.noreply.github.com>
2024-06-25 13:00:49 -0400

    models.py update

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,17 +4,13 @@ from . import views
 urlpatterns = [
 
     # Creamos el path para homepage "Views"
-    path('', views.home, name=""),
+    path('', views.home, name="home"),
 
-    path('register', views.register, name='register' ),
+    path('register', views.register, name='register'),
 
-    path('my_login', views.my_login, name="my-login" ),
+    path('my_login', views.my_login, name="my-login"),
 
     path('user-logout', views.user_logout, name="user-logout"),
-    
-    path('dashboard', views.dashbaord, name="dashboard"),
 
-    
-
-
+    path('dashboard', views.dashboard, name="dashboard"),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,77 +1,44 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
-
-from django.contrib.auth import authenticate, login as auth_login
-
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
 
 # ----------- Creamos la función de HomePage
 def home(request): 
-
-    # llamamos de template el template creado llamado index.html 
     return render(request, 'webapp/index.html')
 
 # ---------- Creamos la función de Register User
 def register(request):
-
     form = CreateUserForm()
-
     if request.method == "POST":
-
         form = CreateUserForm(request.POST) 
-
         if form.is_valid():
-
             form.save()
-
-            # return redirect('')
-
+            return redirect('my-login')
     context = {'form': form}
-
     return render(request, 'webapp/register.html', context=context)
 
 # ---------- Login a user
 def my_login(request):
-
     form = LoginForm()
-
     if request.method == "POST":
-
         form = LoginForm(request, data=request.POST)
-
         if form.is_valid():
-
             username = request.POST.get('username')
             password = request.POST.get('password')
-
             user = authenticate(request, username=username, password=password)
-
             if user is not None:
-
                 auth_login(request, user)
-
                 return redirect('dashboard')
-
     context = {'form': form}
-
     return render(request, 'webapp/my-login.html', context=context)
 
-
 # - Dashboard
-
 @login_required(login_url='my-login')
 def dashboard(request):
-
     return render(request, 'webapp/dashboard.html')
 
-
-
-
-
 #  ----------------------  User Logout
-
 def user_logout(request):
-    
-    auth.logout(request)
-
+    auth_logout(request)
     return redirect("my-login")
