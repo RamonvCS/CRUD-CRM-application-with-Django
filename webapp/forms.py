f0220a5b9cce6a9cc3c9c6b1a0ddbd25a1b8d592
Ramon Valentin <101585464+RamonvCS@users.noreply.github.com>
2024-06-30 23:50:15 -0400

    froms AddRecirdForm created

--- webapp/forms.py
+++ webapp/forms.py
@@ -6,9 +6,12 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms.widgets import PasswordInput, TextInput
 
+from .models import Record
+
 # register/Create a user
 
 class CreateUserForm(UserCreationForm):
+
     class Meta:
         
         model = User
@@ -21,3 +24,12 @@ class LoginForm(AuthenticationForm):
 
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
+
+# 
+
+class AddRecordForm(forms.ModelForm):
+    
+    class Meta:
+        
+        model = User
+        fields = ['username', 'password1', 'password2']
