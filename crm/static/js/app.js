f7f40b26fe457d5791e153b9bf66145b74a0c494
Ramon Valentin <101585464+RamonvCS@users.noreply.github.com>
2024-07-08 20:03:56 -0400

    reigster updated

--- crm/static/js/app.js
+++ crm/static/js/app.js
@@ -1,2 +1,10 @@
 // crm/static/js/app.js
 console.log('app.js is loaded');
+
+// Function to hide the message after 2 seconds
+setTimeout(function() {
+    var message = document.getElementById("message");
+    if (message) {
+        message.style.display = "none";
+    }
+}, 2000); // 2000 milliseconds = 2 seconds
